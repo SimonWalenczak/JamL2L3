5c65b5de2223360f28fd6dd0735afd13353ee705
SimonWalenczak <92367148+SimonWalenczak@users.noreply.github.com>
2022-11-21 15:10:07 +0100

    azre

---
+++ Assets/Scripts/InGame/GameData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameData : MonoBehaviour
+{
+    [SerializeField] float ShootPower = 1;
+    [SerializeField] int MultipScoring = 1;
+    [SerializeField] int _coolDownUltReduc = 0; //per second
+}
