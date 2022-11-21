e95e33b618f834198e038e0941b02e2bdf86e204
Gatien <78294761+GatienDuh@users.noreply.github.com>
2022-11-21 12:02:21 +0100

    Chronometre

---
+++ Assets/Scripts/InGame/Chrono.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Chrono : MonoBehaviour
+{
+    private float chronoTime;
+
+    [Header("Text GameObject")]
+    [SerializeField] private TextMeshProUGUI chronoTxt;
+
+    void Start()
+    {
+        chronoTime = 0;
+        DisplayTime(chronoTime, chronoTxt);
+        StartCoroutine(countSeconds());
+    }
+
+    IEnumerator countSeconds()
+    {
+        yield return new WaitForSeconds(1f);
+        
+        chronoTime++;
+        DisplayTime(chronoTime, chronoTxt);
+        StartCoroutine(countSeconds());      
+    }
+
+    void DisplayTime(float timeToDisplay, TextMeshProUGUI display)
+    {
+        if (timeToDisplay < 0)
+        {
+            timeToDisplay = 0;
+        }
+
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        display.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
