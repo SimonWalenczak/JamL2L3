86efebec959ef56630e309bd78ec4b1590bf1c31
SimonWalenczak <92367148+SimonWalenczak@users.noreply.github.com>
2022-11-21 14:47:14 +0100

    enemy prefab

---
+++ Assets/Scripts/InGame/UiManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UiManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _xpText;
+    [SerializeField] private Image _lifeBar;
+    [SerializeField] private TextMeshProUGUI _killText;
+    [SerializeField] private TextMeshProUGUI _coolDownUltText;
+}
