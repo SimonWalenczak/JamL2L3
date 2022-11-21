2333afea2b8baed650a73db94cd7af839d1ad36c
Gatien <78294761+GatienDuh@users.noreply.github.com>
2022-11-21 14:30:59 +0100

    Player

---
+++ Assets/Scripts/InGame/BasicAttack.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicAttack : MonoBehaviour
+{
+    [Header("Attack Variables")]
+    [SerializeField] private LayerMask enemies;
+    [SerializeField] private float attackForce;
+
+    private Transform player;
+
+    private void Start()
+    {
+        player = GetComponentInParent<Transform>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == enemies)
+        {
+            Vector3 direction = collision.gameObject.transform.position - player.position;
+            
+            if (direction.sqrMagnitude > 0)
+            {
+                direction.Normalize();
+                collision.GetComponent<Rigidbody2D>().AddForce(direction * attackForce, ForceMode2D.Impulse);
+            }
+        }       
+    }
+}
---
+++ Assets/Scripts/InGame/PlayerAttack.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    [Header("Attack Variables")]
+    [SerializeField] private GameObject attackArea;
+    private bool isAttacking;
+    [SerializeField] private float timeToAttack;
+    [SerializeField] private float timeBetweenAttacks;
+
+    private float timerAttack = 0;
+    private float timerBetweenAttacks = 0;
+
+    void Update()
+    {
+        BasicAttackTimer();
+    }
+
+    private void BasicAttack()
+    {
+        isAttacking = true;
+        attackArea.SetActive(true);
+    }
+
+    private void BasicAttackTimer()
+    {
+        if (isAttacking)
+        {
+            timerAttack += Time.deltaTime;
+
+            if (timerAttack >= timeToAttack)
+            {
+                timerAttack = 0;
+                isAttacking = false;
+                attackArea.SetActive(false);
+            }
+        }
+        else if (!isAttacking)
+        {
+            timerBetweenAttacks += Time.deltaTime;
+
+            if (timerBetweenAttacks >= timeBetweenAttacks)
+            {
+                timerBetweenAttacks = 0;
+                BasicAttack();
+            }
+        }
+    }
+}
